c56f9a9a7717a3fdf439197be66631e7e80d5554
Ethan-M <Ethan.meder@yahoo.com>
2018-10-26 17:51:27 -0500

    Testing for the same error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushBot.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import
-@Autonomous(Name='Autonomous',group='Autonomous');
-public class PushBot extends LinearOpMode {
-
-}
